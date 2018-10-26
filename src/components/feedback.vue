516113e236455a4d64bc5fd314790a9fe0412e26
vadim.novikov <ijcnvv@gmail.com>
2018-10-26 14:07:22 +0700

    finished feedback component

--- src/components/feedback.vue
+++ src/components/feedback.vue
@@ -4,15 +4,55 @@ include ../tools/mixins.pug
   v-layout.row.inner.justify-center
     .inner.main__title-wrap
       h2.main__title Отзывы
+  .inner.slider
+    +e.SLICK.list(ref="slick" :options="slickOptions" :key="cityName")
+      +e.wrap(v-for="(item, index) in feedbacks" :key="index")
+        +e.item
+          +e.img(:style="'background-image: url(' + item.img + ')'")
+          +e.quote._t
+            v-icon(large) format_quote
+          +e.container
+            +e.H3.title {{ item.name }}
+            +e.text {{ item.desc }}
+          +e.quote._b
+            v-icon(large) format_quote
 </template>
 
 <script>
 import {mapGetters} from 'vuex'
+import Slick from 'vue-slick'
 
 export default {
-  ...mapGetters({
-    feebbackList: 'feedback/list'
-  })
+  components: {
+    Slick
+  },
+
+  data: () => ({
+    swiping: false,
+    slickOptions: {
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 7000,
+      dots: true,
+      arrows: false,
+      adaptiveHeight: true,
+      infinite: false,
+      dotsClass: 'slider__dots'
+    }
+  }),
+
+  computed: {
+    ...mapGetters({
+      list: 'feedback/list',
+      city: 'cities/current'
+    }),
+    cityName () {
+      return this.city.text
+    },
+    feedbacks () {
+      return this.list.filter(item => item.city == this.cityName)
+    }
+  }
 }
 </script>
 
--- src/store/modules/feedback.js
+++ src/store/modules/feedback.js
@@ -4,23 +4,20 @@ export default {
     list: [{
       name: 'user 1',
       img: '/img/1.jpg',
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam?',
-      city: 'Абакан',
-      source: 'vk'
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam? Laboriosam nisi est debitis, beatae error fugiat molestias voluptas numquam iusto aliquam ipsum eum ex quam? Repellat cum esse deserunt nihil voluptate excepturi. Cumque dolores nam ipsa. Voluptas cum voluptatum quos? Sequi, quod. Accusantium amet veritatis velit!',
+      city: 'Абакан'
     },
     {
       name: 'user 2',
       img: '/img/2.jpg',
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam?',
-      city: 'Абакан',
-      source: 'ok'
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam? Laboriosam nisi est debitis, beatae error fugiat molestias voluptas numquam iusto aliquam ipsum eum ex quam? Repellat cum esse deserunt nihil voluptate excepturi. Cumque dolores nam ipsa. Voluptas cum voluptatum quos? Sequi, quod. Accusantium amet veritatis velit!',
+      city: 'Абакан'
     },
     {
       name: 'user 3',
       img: '/img/3.jpg',
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam?',
-      city: 'Тюмень',
-      source: 'ok'
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo aspernatur alias numquam? Laboriosam nisi est debitis, beatae error fugiat molestias voluptas numquam iusto aliquam ipsum eum ex quam? Repellat cum esse deserunt nihil voluptate excepturi. Cumque dolores nam ipsa. Voluptas cum voluptatum quos? Sequi, quod. Accusantium amet veritatis velit!',
+      city: 'Тюмень'
     }]
   },
   getters: {
