073f8cc8d5bedb8d27d0dedd34eeb519bd32952a
vadim.novikov <ijcnvv@gmail.com>
2018-11-06 22:29:56 +0700

    did gallery module

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -77,7 +77,7 @@ export default {
   computed: {
     ...mapGetters({
       list: 'gallery/currenCityList',
-      city: 'cities/currentName'
+      city: 'cities/currentId'
     }),
 
     gallery () {
--- src/store/modules/gallery.js
+++ src/store/modules/gallery.js
@@ -2,80 +2,119 @@ export default {
   namespaced: true,
   state: {
     list: [{
-      src: '/img/gallery/akb(1).jpg',
-      preview: '/img/gallery/akb(1)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_1.jpg',
+      preview: '/img/gallery/akb_1_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb(2).jpg',
-      preview: '/img/gallery/akb(2)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_2.jpg',
+      preview: '/img/gallery/akb_2_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb(3).jpg',
-      preview: '/img/gallery/akb(3)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_3.jpg',
+      preview: '/img/gallery/akb_3_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb (4).jpg',
-      preview: '/img/gallery/akb (4)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_4.jpg',
+      preview: '/img/gallery/akb_4_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb (5).jpg',
-      preview: '/img/gallery/akb (5)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_5.jpg',
+      preview: '/img/gallery/akb_5_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb (6).jpg',
-      preview: '/img/gallery/akb (6)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_6.jpg',
+      preview: '/img/gallery/akb_6_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/akb (7).jpg',
-      preview: '/img/gallery/akb (7)_1.jpg',
-      city: 'Абакан'
+      src: '/img/gallery/akb_7.jpg',
+      preview: '/img/gallery/akb_7_1.jpg',
+      city: 1
     },
     {
-      src: '/img/gallery/tmn (1).jpg',
-      preview: '/img/gallery/tmn (1)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_1.jpg',
+      preview: '/img/gallery/tmn_1_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (2).jpg',
-      preview: '/img/gallery/tmn (2)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_2.jpg',
+      preview: '/img/gallery/tmn_2_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (3).jpg',
-      preview: '/img/gallery/tmn (3)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_3.jpg',
+      preview: '/img/gallery/tmn_3_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (4).jpg',
-      preview: '/img/gallery/tmn (4)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_4.jpg',
+      preview: '/img/gallery/tmn_4_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (5).jpg',
-      preview: '/img/gallery/tmn (5)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_5.jpg',
+      preview: '/img/gallery/tmn_5_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (6).jpg',
-      preview: '/img/gallery/tmn (6)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_6.jpg',
+      preview: '/img/gallery/tmn_6_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (7).jpg',
-      preview: '/img/gallery/tmn (7)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_7.jpg',
+      preview: '/img/gallery/tmn_7_1.jpg',
+      city: 2
     },
     {
-      src: '/img/gallery/tmn (8).jpg',
-      preview: '/img/gallery/tmn (8)_1.jpg',
-      city: 'Тюмень'
+      src: '/img/gallery/tmn_8.jpg',
+      preview: '/img/gallery/tmn_8_1.jpg',
+      city: 2
+    },{
+      src: '/img/gallery/spb_1.jpg',
+      preview: '/img/gallery/spb_1_1.jpg',
+      city: 3
     },
+    {
+      src: '/img/gallery/spb_2.jpg',
+      preview: '/img/gallery/spb_2_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_3.jpg',
+      preview: '/img/gallery/spb_3_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_4.jpg',
+      preview: '/img/gallery/spb_4_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_5.jpg',
+      preview: '/img/gallery/spb_5_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_6.jpg',
+      preview: '/img/gallery/spb_6_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_7.jpg',
+      preview: '/img/gallery/spb_7_1.jpg',
+      city: 3
+    },
+    {
+      src: '/img/gallery/spb_8.jpg',
+      preview: '/img/gallery/spb_8_1.jpg',
+      city: 2
+    }
     ]
   },
   getters: {
